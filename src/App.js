750db6c0f6775a9e00943b060905ebb97c636209
Hilal Ahmad <ihilalahmadd@gmail.com>
2022-06-08 17:40:27 +0500

    some new features added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+import About from "./components/About";
 
 function App() {
   return (
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   //   setText("Hilal ahmad is MERN Stack developer");
 
   const convertToUpperCase = () => {
@@ -14,29 +14,63 @@ export default function Textform(props) {
     setText(newText);
   };
 
+  const clearText = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const copyText = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const onTextChange = (event) => {
     console.log("Text changing....");
     setText(event.target.value);
   };
 
   return (
-    <div>
-      <div className="mb-3">
-        <h2>{props.heading}</h2>
-        <textarea
-          className="form-control"
-          value={text}
-          id="myTextBox"
-          rows="3"
-          onChange={onTextChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h2>{props.heading}</h2>
+          <textarea
+            className="form-control mt-3"
+            value={text}
+            id="myTextBox"
+            rows="8"
+            onChange={onTextChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary me-2" onClick={convertToUpperCase}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary me-2" onClick={convertToLowerCase}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-danger me-2" onClick={clearText}>
+          Clear text
+        </button>
+        <button className="btn btn-success me-2" onClick={copyText}>
+          Copy text
+        </button>
+        <button className="btn btn-warning " onClick={removeExtraSpaces}>
+          Remove extra spaces
+        </button>
       </div>
-      <button className="btn btn-primary me-2" onClick={convertToUpperCase}>
-        Convert to Uppercase
-      </button>
-      <button className="btn btn-primary " onClick={convertToLowerCase}>
-        Convert to Lowercase
-      </button>
-    </div>
+      <div className="contianer mt-5">
+        <h2>Your text summary:</h2>
+        <p>
+          {text.split(" ").length - 1} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
